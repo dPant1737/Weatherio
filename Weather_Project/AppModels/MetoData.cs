f22ebff8c10541e8003de6ba0c9f28f79a349bee
dPant1737 <136116690+dPant1737@users.noreply.github.com>
2023-06-27 22:07:07 -0400

    Add files via upload

---
+++ Weather_Project/AppModels/MetoData.cs
@@ -0,0 +1,17 @@
+public class MetoData{
+public double Temperature { get; set; }
+
+public int Humidity { get; set; }
+
+public double WindSpeed { get; set; }
+
+public double Precipitation { get; set; }
+
+public double Snow { get; set; }
+
+public double Rain { get; set; }
+
+public double Showers { get; set; }
+
+public string Time { get; set; }
+}
